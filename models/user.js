7cf82b0f4aafc593e924064d0ce0ad91764d9545
Jacob Regan <jacobmregan0@gmail.com>
2017-05-07 22:03:46 -0500

    Finished Exercise

--- models/user.js
+++ models/user.js
@@ -2,9 +2,31 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+  first: {
+    type: Sequelize.STRING},
+  last: {
+    type: Sequelize.STRING},
+  age: {
+    type: Sequelize.INTEGER, validate: {min: 18}},
+  email: {
+    type: Sequelize.STRING, allowNull: false},
+  bio: {
+    type: Sequelize.TEXT}
 }, {
-// ...AND HERE
+  getterMethods: {
+    fullName: function(){
+      return this.first + ' ' + this.last;
+    }
+  },
+  instanceMethods: {
+    haveBirthday: function(){
+      this.age++;
+      return this.save()
+      .then(function(user){
+        return user
+      })
+    }
+  }
 });
 
 module.exports = User;
